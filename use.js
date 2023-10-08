ad9d859e4625cfe448e8870f1b1c6e65309fca6b
Laiba Ahmad <134959431+Laiba-Ahmad@users.noreply.github.com>
2023-10-08 22:17:43 +0500

    Update use.js

--- use.js
+++ use.js
@@ -4,8 +4,7 @@ document.write("<p>Made By Laiba</p>")
 Where there are round brackets, it will be a function
 // TRANSLATE
 جہاں پر راؤنڈ بریکٹ ہوں گے تو وہ فنکشن ہوگا
-    
-    
+        
 // Example
 document.write("2"+"Laiba")
 
@@ -17,9 +16,13 @@ agar string hua to usko concat kardega
 // TRANSLATE
 اگر وہ سٹرنگ ہوگا تو وہ اس کو کون کیٹ کر دے گا
 
-
 // WHAT IS STRING
 string ==> "Hello World"
 
 // WHAT IS INTEGER
 integer ==> 1,2,3,4,5,6,7,8,9,10
+
+// WHAT IS ALERT
+Alert is used to show a message to the user
+// TRANSLATE
+الرٹ یوزر کو میسج شو کروانے کے لیے استعمال ہوتا ہے
