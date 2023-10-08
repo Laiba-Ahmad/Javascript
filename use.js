33a67e5e21fb07836d9aa7777ce97a6b86aa8b3f
Laiba Ahmad <134959431+Laiba-Ahmad@users.noreply.github.com>
2023-10-08 22:12:25 +0500

    Update use.js

--- use.js
+++ use.js
@@ -1,1 +1,25 @@
+document.write("<p>Made By Laiba</p>")
 
+// WHAT IS Function
+Where there are round brackets, it will be a function
+// TRANSLATE
+جہاں پر راؤنڈ بریکٹ ہوں گے تو وہ فنکشن ہوگا
+    
+    
+// Example
+document.write("2"+"Laiba")
+
+// + see two things 
+If it is an integer, the operation will be performed
+// TRANSLATE
+
+agar string hua to usko concat kardega
+// TRANSLATE
+اگر وہ سٹرنگ ہوگا تو وہ اس کو کون کیٹ کر دے گا
+
+
+// WHAT IS STRING
+string ==> "Hello World"
+
+// WHAT IS INTEGER
+integer ==> 1,2,3,4,5,6,7,8,9,10
