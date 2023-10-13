9058c08c06345fb6295ef835b0e2d9c5353b814f
Laiba Ahmad <134959431+Laiba-Ahmad@users.noreply.github.com>
2023-10-13 20:17:00 +0500

    Create about.js

---
+++ about.js
@@ -0,0 +1,62 @@
+document.write("<p>Made By Laiba</p>")
+
+// WHAT IS JavaScript
+JavaScript is the Programming Language for the Web.
+JavaScript can update and change both HTML and CSS.
+JavaScript can calculate, manipulate and validate data.
+
+// What is JavaScript and why it is used?
+JavaScript is a text-based programming language used both on the client-side and server-side that allows you to make web pages interactive. Where HTML and CSS are languages that give structure and style to web pages, JavaScript gives web pages interactive elements that engage a user.
+
+// What is JavaScript explain?
+JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, ...        
+
+// WHAT IS Function
+Where there are round brackets, it will be a function
+// TRANSLATE
+جہاں پر راؤنڈ بریکٹ ہوں گے تو وہ فنکشن ہوگا
+        
+// Example
+document.write("2"+"Laiba")
+
+// + see two things 
+If it is an integer, the operation will be performed
+// TRANSLATE
+
+agar string hua to usko concat kardega
+// TRANSLATE
+اگر وہ سٹرنگ ہوگا تو وہ اس کو کون کیٹ کر دے گا
+
+// WHAT IS STRING
+string ==> "Hello World"
+
+// WHAT IS INTEGER
+integer ==> 1,2,3,4,5,6,7,8,9,10
+
+// WHAT IS ALERT
+Alert is used to show a message to the user
+// TRANSLATE
+الرٹ یوزر کو میسج شو کروانے کے لیے استعمال ہوتا ہے
+Alerts are also used by us to show messages or perform operations
+
+Three methods to print
+1) document.write
+2) alert
+3) console.log
+
+var ==> variable
+
+// How many Case in js
+There are 3 case in js
+1) Camel Case
+2) Snake Case
+3) Pascal Case
+
+// HOW MANY case-sensitive IN JS
+There are two types of case-sensitive.
+1) Lower Case
+2) Upper Case
+
+// What is  parseInt
+1. A string to convert into a number
+2.Converts a string to an integer
