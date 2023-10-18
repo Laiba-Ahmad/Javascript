4df16e52733b16d10d08955a8e3feaff743e0fa6
Laiba Ahmad <134959431+Laiba-Ahmad@users.noreply.github.com>
2023-10-18 19:55:37 +0500

    Perform operation on app.js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+alert("Sum")
+var num1 = +prompt("Enter Your Number 1")
+var num2 = +prompt("Enter Your Number 2")
+var num3 = num1+num2
+var num4 = num3
+document.write(num4)
 
+alert("Sub")
+var num1 = +prompt("Enter Your Number 1")
+var num2 = +prompt("Enter Your Number 2")
+var num3 = num1-num2
+var num4 = num3
+document.write(num4)
+
+alert("Mul")
+var num1 = +prompt("Enter Your Number 1")
+var num2 = +prompt("Enter Your Number 2")
+var num3 = num1*num2
+var num4 = num3
+document.write(num4)
+
+alert("Div")
+var num1 = +prompt("Enter Your Number 1")
+var num2 = +prompt("Enter Your Number 2")
+var num3 = num1/num2
+var num4 = num3
+document.write(num4)
