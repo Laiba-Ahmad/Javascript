b925aa6c68bbbb9098cc5124f5f225c15fc507b9
Laiba Ahmad <134959431+Laiba-Ahmad@users.noreply.github.com>
2023-11-10 07:46:04 -0800

    Update about.js

--- about.js
+++ about.js
@@ -6,68 +6,83 @@ JavaScript can update and change both HTML and CSS.
 JavaScript can calculate, manipulate and validate data.
 // WHAT IS JavaScript END
 
-// What is JavaScript and why it is used?
+// What is JavaScript and why it is used?         START
 JavaScript is a text-based programming language used 
 both on the client-side and server-side that allows 
 you to make web pages interactive. Where HTML and CSS 
 are languages that give structure and style to web pages,
 JavaScript gives web pages interactive elements that engage
 a user.
+// What is JavaScript and why it is used?         END
 
-// What is JavaScript explain?
+// What is JavaScript explain?         START
 JavaScript is a scripting or programming language that allows
 you to implement complex features on web pages — every time a
 web page does more than just sit there and display static 
 information for you to look at — displaying timely content 
 updates, interactive maps, animated 2D/3D graphics, scrolling
 video jukeboxes, ...        
+// What is JavaScript explain?         END
 
-// WHAT IS Function
+// WHAT IS Function         START
 Where there are round brackets, it will be a function
+// WHAT IS Function         END
         
-// Example
+// Example         START
 document.write("2"+"Laiba")
+// Example         END
 
-// + see two things 
+// + see two things          START
 If it is an integer, the operation will be performed
 
 agar string hua to usko concat kardega
+// + see two things          END
 
-// WHAT IS STRING
+// WHAT IS STRING         START
 string ==> "Hello World"
+// WHAT IS STRING         END
 
-// WHAT IS INTEGER
+// WHAT IS INTEGER         START
 integer ==> 1,2,3,4,5,6,7,8,9,10
+// WHAT IS INTEGER         END
 
-// WHAT IS ALERT
+// WHAT IS ALERT         START
 Alert is used to show a message to the user
 
 Alerts are also used by us to show messages or perform
 operations
+// WHAT IS ALERT         END
 
+// Printing Methods         START
 Three methods to print
 1) document.write
 2) alert
 3) console.log
+// Printing Methods         END
 
 var ==> variable
 
-// How many Case in js
+// How many Case in js         START
 There are 3 case in js
 1) Camel Case
 2) Snake Case
 3) Pascal Case
+// How many Case in js         END
 
-// HOW MANY case-sensitive IN JS
+// HOW MANY case-sensitive IN JS         START
 There are two types of case-sensitive.
 1) Lower Case
 2) Upper Case
+// HOW MANY case-sensitive IN JS         END
 
-// What is  parseInt
+// What is  parseInt         START
+
 1. A string to convert into a number
 2.Converts a string to an integer
 
-// These Sings do
+// What is  parseInt         END
+
+// These Sings do         START
 
 =        assign value 
 ==       equal
@@ -77,8 +92,9 @@ There are two types of case-sensitive.
 <=       less equal
 &&       AND
 ||       OR
+// These Sings do         END
 
-// Example if start 
+// Example if START 
 
 
-// Example if end
+// Example if END
